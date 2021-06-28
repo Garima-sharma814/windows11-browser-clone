6b293ba7606de677ce568c384ea8b0d2ae4e943c
Garima <65615150+Garima-sharma814@users.noreply.github.com>
2021-06-28 11:13:16 +0530

    add: animations and hover effects for taskbar

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var windowicon = document.getElementById('window-icon');
 var startmenu = document.getElementById('startmenu');
+var tasks= document.getElementsByClassName('tasks')[0];
+
 
 windowicon.addEventListener('click', ()=>{
     if(startmenu.style.bottom === "50px"){
-        startmenu.style.bottom = "-550px"
+        startmenu.style.bottom = "-550px";
+        tasks.style.background = "none";
+
     }else{
         startmenu.style.bottom = "50px"
+        tasks.style.backgroundColor = "rgb(196, 195, 195)";
     }
 });
